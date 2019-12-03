32cbdbf2932e74d5a096d7c948c9dc5f4d7cb938
endormi <ernosalo14@gmail.com>
2019-12-03 16:26:39 +0200

    Add { Card } and update gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,43 +2,70 @@ import React, { Component } from 'react';
 import './App.css';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
 import Navbar from './components/Navbar';
 
 class App extends Component {
-  state = {
-    faangm: []
-  };
+    state = {
+        faangm: [],
+    };
 
-  async componentDidMount() {
-    try {
-      const res = await fetch('http://127.0.0.1:8000/faangm/');
-      const faangm = await res.json();
-      this.setState({
-        faangm
-      });
-    } catch (e) {
-      console.log(e);
+    async componentDidMount() {
+        try {
+            const res = await fetch('http://127.0.0.1:8000/faangm/');
+            const faangm = await res.json();
+            this.setState({
+                faangm,
+            });
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
 
-  render() {
-    return (
-      <div className="container">
-      <Navbar />
-      <br />
-      <h1>Faangm Companies</h1>
-      <p>See the list to understand what it means.</p>
-        {this.state.faangm.map(item => (
-          <div key={item.id}>
-            <h3>{item.name}</h3>
-            <span className="text-size">{item.created}</span>
-            <p>{item.location}</p>
-            <br />
-          </div>
-        ))}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <Navbar />
+                <br />
+                <h1>Faangm Companies</h1>
+                <p>See the list to understand what it means.</p>
+                {this.state.faangm.map(item => {
+                    return (
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <Card style={{ margin: `10px` }}>
+                                    <Card.Header
+                                        style={{
+                                            textAlign: `center`,
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <Card.Text
+                                            style={{
+                                                textAlign: `center`,
+                                            }}
+                                        >
+                                            {item.created}
+                                        </Card.Text>
+                                        <Card.Text
+                                            style={{
+                                                color: `red`,
+                                                textAlign: `center`,
+                                            }}
+                                        >
+                                            {item.location}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
