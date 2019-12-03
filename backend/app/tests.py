711f41513520e058cd00c6fb3c3ff6fa38a1c12d
endormi <ernosalo14@gmail.com>
2019-12-03 17:15:13 +0200

    Add tests

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class LangTests(APITestCase):
+    def test_languages_url(self):
+        """
+        Ensure we can get an object.
+        """
+        url = ('/languages/')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_languages_url_post(self):
+        """
+        Ensure we can create a new object.
+        """
+        data = {'name': 'C', 'paradigm': 'Imperative (procedural), structured', 'created_by': 'Dennis Ritchie'}
+        url = ('/languages/')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class FaangmTests(APITestCase):
+    def test_faangm_url(self):
+        """
+        Ensure we can get an object.
+        """
+        url = ('/faangm/')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_faangm_url_post(self):
+        """
+        Ensure we can create a new object.
+        """
+        data = {'name': 'Pied Piper', 'created': '2014', 'location': 'Erlich Bachman\'s business incubator'}
+        url = ('/faangm/')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
