0b4f580cb86618571921d540d27cc7312b7fe457
Erno Salo <39559256+endormi@users.noreply.github.com>
2019-12-30 07:38:53 +0200

    Use only django_migrations

--- backend/data.sql
+++ backend/data.sql
@@ -1,14 +1,1 @@
 SELECT "django_migrations"."id", "django_migrations"."app", "django_migrations"."name", "django_migrations"."applied" FROM "django_migrations"
-SELECT 'session_stats' AS chart_name, row_to_json(t) AS chart_data
-FROM (SELECT
-   (SELECT count(*) FROM pg_stat_activity WHERE datname = (SELECT datname FROM pg_database WHERE oid = 16396)) AS "Total",
-   (SELECT count(*) FROM pg_stat_activity WHERE state = 'active' AND datname = (SELECT datname FROM pg_database WHERE oid = 16396))  AS "Active",
-   (SELECT count(*) FROM pg_stat_activity WHERE state = 'idle' AND datname = (SELECT datname FROM pg_database WHERE oid = 16396))  AS "Idle"
-) t
-UNION ALL
-SELECT 'tps_stats' AS chart_name, row_to_json(t) AS chart_data
-FROM (SELECT
-   (SELECT sum(xact_commit) + sum(xact_rollback) FROM pg_stat_database WHERE datname = (SELECT datname FROM pg_database WHERE oid = 16396)) AS "Transactions",
-   (SELECT sum(xact_commit) FROM pg_stat_database WHERE datname = (SELECT datname FROM pg_database WHERE oid = 16396)) AS "Commits",
-   (SELECT sum(xact_rollback) FROM pg_stat_database WHERE datname = (SELECT datname FROM pg_database WHERE oid = 16396)) AS "Rollbacks"
-) 
