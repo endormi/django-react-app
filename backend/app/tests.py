dd9a04808f29078f08ef09d4b0686f94aa2ad9da
endormi <ernosalo14@gmail.com>
2020-01-04 18:47:07 +0200

    Fix post test datetime with testfixtures

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
+from testfixtures.tests.sample1 import str_today_1
 
 
 class LangTests(APITestCase):
@@ -31,11 +32,9 @@ class FaangmTests(APITestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-"""
     def test_faangm_url_post(self):
         # Ensure we can create a new object.
-        data = {'name': 'Pied Piper', 'founders': 'Richard Hendricks', 'created': '04/06/2014', 'location': 'Erlich Bachman\'s business incubator'}
+        data = {'name': 'Pied Piper', 'founders': 'Richard Hendricks', 'created': str_today_1(), 'location': 'Erlich Bachman\'s business incubator'}
         url = ('/faangm/')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-"""
